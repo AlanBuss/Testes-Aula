bb61581bb6e1d4e907e97fe817119d79abb2b6c2
Alan Buss <55300724+AlanBuss@users.noreply.github.com>
2022-08-09 18:47:25 -0300

    Create c1.c

---
+++ c1.c
@@ -0,0 +1,14 @@
+int main(int argc, char const *argv[])
+{
+    int contador;
+    printf("\nDigite um numero: ");
+    scanf("%d", &contador);
+
+    for(contador;contador >=1; contador--)
+    {
+        printf("%d", contador);
+    }
+
+    return 0;
+    
+}
