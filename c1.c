6239d3e646c819f9d1a542e50e3ae898a94560e4
Alan Buss <55300724+AlanBuss@users.noreply.github.com>
2022-08-09 18:53:42 -0300

    Update c1.c

--- c1.c
+++ c1.c
@@ -12,3 +12,29 @@ int main(int argc, char const *argv[])
     return 0;
     
 }
+//////////////////////////////////////////////////////////////////////////////////////////////////////// TESTE 2
+
+
+int main(int argc, char const *argv[])
+{
+   int linha, coluna;
+   int matriz[2][2];
+   for(linha=0; coluna<2; linha++)
+
+   {
+      for(coluna=0; coluna<2; coluna++)
+      {
+        printf("Digite os valores da matriz para linha %d, coluna %d:", linha+1, coluna+1);
+         scanf("%d", &matriz[linha][coluna]);
+         for(linha=0; linha<2; linha++){
+            for(coluna=0; coluna<2; coluna++){
+               printf("%d\t", matriz[linha][coluna]);
+            }
+            printf(""Veja a sua matriz");
+            printf("\n\n");
+            
+         }
+      }
+    return 0;
+
+}
